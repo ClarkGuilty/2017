d63e82ce4a056e619e7baedd56fd5374c767a3a4
Javier Acevedo <Javier Acevedo>
2017-02-06 20:41:46 -0500

    12 pjEu

--- metodos/euler.py
+++ metodos/euler.py
@@ -1,5 +1,5 @@
 
-import math
+from math import *
 
 
 def prime(a):
@@ -33,6 +33,72 @@ def nDigitsFib(n):
 		h = math.floor(h)
 		i = h + 1
 		print("Voy en el elemento %d con  %d digitos." % (j,i))
+
+
+def pDiv(n):
+	if(prime(n)):
+		return [[n], [1]]
+	else:
+		pDiv = []
+		nDiv = []
+		if(n%2 == 0):
+			pDiv.append(2)
+			q = 1; n = n/2
+			while(n%2 == 0):
+				q += 1
+				n = n/2
+			nDiv.append(q)
+		cuenta = 3
+		while(n != 1 ):
+			if(n%cuenta == 0) :
+				pDiv.append(cuenta)
+				q = 1; n = n/cuenta
+				while(n%cuenta == 0):
+					q += 1
+					n = n/cuenta
+				nDiv.append(q)
+			cuenta += 2
+		return [pDiv, nDiv]
+
+
+def triang(n):
+	return n * (n+1) / 2
+
+def nDiv( heh):
+	a = 0
+	total=1
+	while(a < len(heh)):
+		total = (heh[a] +1)*total
+		a += 1
+	return total
+
+
+
+def eu12():
+	q = 3
+	div = 1
+	while(div < 500):
+		q = q+1
+		a = triang(q)
+		rta = pDiv(a)
+		div = nDiv(rta[1])
+		print("vamos en la posicion %d, el numero %d, con %d divisores" %(q, a, div))
+	print("la respuesta es el %d, con %d divisores" %(a,div))
+
+def dir(n):
+	return nDiv(pDiv(n)[1])
+
+
+
+print dir(20)
+print dir(28)
+eu12()
+
+
+
+
+
+
 
 
 
