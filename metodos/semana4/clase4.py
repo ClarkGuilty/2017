de7f23e9353cbe2a9f4e95f778f6a472843902c5
ClarkGuilty <ja.acevedo12@uniandes.edu.co>
2017-02-14 16:33:14 -0500

    less rip

--- metodos/semana4/clase4.py
+++ metodos/semana4/clase4.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math as m
+from numpy.linalg import inv 
 
 #Hacer la matriz.
 xp = np.array([0, 0.25, 0.5, 0.75])
@@ -23,13 +24,69 @@ x = np.linspace(-3,3,2000)
 y = serie(x, sol[0], sol[1], sol[2],sol[3])
 
 
-f = plt.plot(x,y)
-plt.plot(xp,yp,'ro')
+#f = plt.plot(x,y)
+#plt.plot(xp,yp,'ro')
 
 
-plt.savefig("heh.png")
+#plt.savefig("heh.png")
+
+#Punto 3
+
+data = np.loadtxt("movimiento.dat")
+
+f = plt.plot(data[:,0], data[:,1], 'ro')
+
+def par(x,a,b,c):
+	return a*x**2 + b*x+c
+
+x2 = data[:,0]
+y2 = data[:,1]
+
+n = len(x2)
+J = np.zeros((n,3))
+k = np.zeros(n)
+
+for i in range(n):
+	J[i] = x2[i]**2,  x2[i], 1
+	k[i] = y2[i]
+
+#J1 = inv(J)
+Jt = np.transpose(J)
+
+kim = np.dot(Jt, J)
+kim1 =inv(kim)
+
+heh = np.dot(kim1, Jt)
+rta = np.dot(heh, k)
 
 
+x = np.linspace(0, 11, 2000)
+print rta
+
+y_fit = par(x, rta[2], rta[1], rta[0])
+plt.plot(x,y)
+
+plt.savefig("par.png")
+
+#print k[1] 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
 
 
 
