03647cd66b5891156bccdca0f93e2e1c9dd0775c
Susana <nosoybatmam@gmail.com>
2017-04-04 14:18:19 -0500

    Susy es amor

--- metodos/tareas/tarea3/FourierAnalysis.py
+++ metodos/tareas/tarea3/FourierAnalysis.py
@@ -2,36 +2,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from scipy.fftpack import fft, fftfreq
+from matplotlib.figure import SubplotParams as sp
 
 
 
 
 data0 = wavfile.read("trumpet.wav")
+data1 = wavfile.read("violin.wav")
 
-v = data0[0]
-x = data0[1]
-
-
-f0 = fft(x)/len(x)
-w = fftfreq(len(x), len(x)/v)
-f = plt.plot(f0.real,w)
-plt.savefig('satan')
+v0 = data0[0]
+x0 = data0[1]
+v1 = data1[0]
+x1 = data1[1]
 
 
+f0 = fft(x0)/len(x0)
+f1 = fft(x1)/len(x1)
+w0 = fftfreq(len(x0), len(x0)/v0)
+w1 = fftfreq(len(x1), len(x1)/v1)
+#f = plt.plot(w0,f0.real)
+#plt.plot(w1,f1.real)
 
 
+he = sp(hspace = 0.5)
+fig = plt.figure(subplotpars = he)
 
+plt.title("")
+    
+ax1 = fig.add_subplot(211)
 
+ax1.plot(w0,f0.real, label = "x^4", color = 'black')
 
+    
+h1 = plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    #plt.savefig("Anomalies_SOI_Plot.png",bbox_extra_artists=(h,), bbox_inches='tight')
 
+ax2 = fig.add_subplot(212)
+    
+    
+ax2.plot(w1,f1, label = "x^4",  color = 'black')
 
+    
+h2= plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-
-
-
-
-
-
+    
+plt.savefig("heh.png",bbox_extra_artists=(h1,h2,), bbox_inches='tight')
 
 
 
