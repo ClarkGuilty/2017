0d6f3a1dcee276f7ec6866a396e723b1d958612b
Cohnan <sebastian.cohnan@gmail.com>
2017-04-21 11:56:38 -0500

    rip ondas

--- otrasMaterias/glory to satan/Sin título 0.py
+++ otrasMaterias/glory to satan/Sin título 0.py
@@ -7,13 +7,14 @@ Created on Thu Apr 20 20:47:46 2017
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 
 
 largo = np.loadtxt("largo.dat")
 pixeles = len(largo[:,0])
-largo = 1.0 #m
-densidad = largo/pixeles
+largo = 0.01 #cm
+densidad = largo/(pixeles-1)
 
 
 fig = plt.figure()
@@ -22,14 +23,32 @@ fig = plt.figure()
 #    data = np.loadtxt("g"+str(i)+".dat")
 #    plt.plot(data[:,0],data[:,1])
 
+#632.8
 
 i = 5
 
-data = np.loadtxt("n"+str(i)+".dat")
-plt.plot(data[:,0],data[:,1], label = ("n"+str(i)+".dat") )
-plt.legend(loc = 3)
-a = 33
-b = 186
-g = a-b
-plt.xlim(a,b)
+#data = np.loadtxt("n"+str(i)+".dat")
+#plt.plot(data[:,0],data[:,1], label = ("n"+str(i)+".dat") )
+#plt.legend(loc = 3)
+#a = 33
+#b = 186
+#g = a-b
+#plt.xlim(a,b)
+
+datag = np.loadtxt("g.txt")
+def lar(x, lamb):
+  return lamb*1.53/0.00015
+
+
+x = datag[:,0]
+g = datag[:,1]*densidad
+
+
+var, diff = curve_fit(lar,x,g)
+
+print x
+print var*1000000000
+print np.sqrt(np.trace(diff))*1000000000
+
+
 
