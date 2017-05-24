d47b5e5733a8a5a25ef295a32d80e11907c504ae
Guest <guest-g5to24@compufi23.uniandes.edu.co>
2017-05-24 18:20:08 -0500

    solo falta un poco

--- metodos/tareas/tarea5/Tarea5.mk
+++ metodos/tareas/tarea5/Tarea5.mk
@@ -1,11 +1,13 @@
-all: resultados.txt clean
+all: Resultados_hw5.pdf clean
 
-resultados.txt: canal_ionico.exe
-	./canal_ionico.exe
+Resultados_hw5.pdf: canal_ionico.c
+	wget https://raw.githubusercontent.com/ClarkGuilty/2017/master/metodos/tareas/datosTarea5/Canal_ionico.txt
+	wget https://raw.githubusercontent.com/ClarkGuilty/2017/master/metodos/tareas/datosTarea5/Canal_ionico1.txt
+	wget https://raw.githubusercontent.com/ClarkGuilty/2017/master/metodos/tareas/datosTarea5/CircuitoRC.txt
+	cc -o canal_ionico.o canal_ionico.c -lm
+	-./canal_ionico.o
 	python plots_canal_ionico.py
-
-canal_ionico.exe: canal_ionico.c
-	gcc -o canal_ionico.exe canal_ionico.c -lm
+	pdflatex Resultados_hw5.tex
 
 clean: 
-	rm -f res1.txt res2.txt g1.png g2.png his1.png his2.png resultados.txt
+	-rm -f res1.txt res2.txt g1.png g2.png his1.png his2.png Canal_ionico.txt Canal_ionico1.txt CircuitoRC.txt canal_ionico.o resultados.txt Resultados_hw5.aux Resultados_hw5.log Resultados_hw5.out
